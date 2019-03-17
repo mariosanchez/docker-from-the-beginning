229396505c66640001a785c87c5fa4084afd6a57
Mario Sánchez <mr.mario.sm@gmail.com>
2019-03-17 19:04:36 +0100

    Add a dummy route to test our docker volume and nodemon

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello Wolrd!'));
 app.get('/docker', (req, res) => res.send('Hello from docker!'));
+app.get('/nodemon', (req, res) => res.send('Hello nodemon!'));
 
 app.listen(port, () => console.log(`Example app is running on http://localhost:${port} !`));
