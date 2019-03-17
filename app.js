ea899e07c55ba7d346edbe731df490a55cc2e07f
Mario Sánchez <mr.mario.sm@gmail.com>
2019-03-17 18:48:11 +0100

    Do some docker volumes experiments

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello Wolrd!'));
+app.get('/docker', (req, res) => res.send('Hello from docker!'));
 
 app.listen(port, () => console.log(`Example app is running on http://localhost:${port} !`));
